2e8e9f09154feeb86857431c34fc3256e675980d
UmmRasheeda <mohammedrasheeda28@gmail.com>
2024-10-29 16:31:26 +0100

    first commit

---
+++ html/javascript/index.js
@@ -0,0 +1,64 @@
+// let department = "Programming";
+// typeof("programming")
+// console.log("programming")
+
+// let age =1;
+// age=2
+// console.log(age)
+
+// let firstname= "Mohammed";
+// console.log(firstname)
+// let lastname= "Rasheeda";
+// console.log(lastname)
+
+// let sum= 200 + 500
+// console.log(sum)
+
+// let division= 100/2
+// console.log(division)
+
+// let multiplication= 100*100
+// console.log(multiplication)
+
+
+// let y=200
+// y *=50;
+// y +=200;
+// console.log(y)
+// // define a variable age and assign the value 19 to it 
+// // check if the age is less than 18, "you are not eligible"
+// // if the age is between 18 and 50, "you are eligible"
+// // if the age is greater than 50, "you are not eligible"
+
+
+// let age = 19;
+// if (age < 18) {
+//     console.log("You are  underage, so you are not eligible")
+// } else if (age >= 18 && age <= 50) {
+// console.log("You are eligible!");
+// } else {
+// console.log("You are too old, so you are not eligible")
+// }
+
+function greetUser(firstname, lastname) {
+    console.log("Hello " + firstname, lastname);
+}
+greetUser("Rasheeda", "Mohammed");
+greetUser("Mohammed","Ali");
+greetUser("Abdulai", "Aisha");
+
+// write a function that performs an addition with the parameters a,b and logs the result.
+
+function addition( a, b ){
+    console.log( a+b);
+}
+addition(10, 5)
+
+let message = 'Hello';
+const greeting = () => {
+
+    console.log(message);
+}
+
+greeting()
+
